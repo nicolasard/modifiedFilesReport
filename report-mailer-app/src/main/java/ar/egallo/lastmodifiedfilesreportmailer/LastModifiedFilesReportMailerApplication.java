1a67b5c643f12e2b8b441d594d9999aa19b2d5fc
vsinicar <nicolas.ard@gmail.com>
2023-01-07 15:31:01 +0100

    Minor fixing

--- report-mailer-app/src/main/java/ar/egallo/lastmodifiedfilesreportmailer/LastModifiedFilesReportMailerApplication.java
+++ report-mailer-app/src/main/java/ar/egallo/lastmodifiedfilesreportmailer/LastModifiedFilesReportMailerApplication.java
@@ -18,10 +18,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 @SpringBootApplication
+@ComponentScan({"ar.nic.influxdb","ar.egallo.lastmodifiedfilesreportmailer"})
 public class LastModifiedFilesReportMailerApplication implements CommandLineRunner {
 
     Logger logger = LoggerFactory.getLogger(LastModifiedFilesReportMailerApplication.class);
